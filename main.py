554ab7da3db57c77f66dd9cc2e130714bc5fe3a9
Matheus Leal Leone <matheusleal.leone@gmail.com>
2023-10-02 18:07:27 -0500

    Attempt to create a more interactive way to see CRUD operations.

--- main.py
+++ main.py
@@ -1,149 +1,156 @@
 # ****************************** #
 # College	: Trebas Institute 
 # Professor	: Iyad Koteich
-# Class		: Edward
-# Day		: 2023-01-10
+# Student	: Edward / Matheus Leal
+# Day		: 02-10-2023
 # ****************************** #
 
-#Importing Libres
-#basis 
+# Imports
 import hashlib, random
+from flask import Flask, make_response, jsonify, request, render_template, redirect, url_for
+import sqlite3	
 
-## Flask ##
-from flask import Flask, make_response, jsonify, request
-
-## sqlite3 ##
-import sqlite3
-from sqlite3 import Error
-
-# Connecting to Database
 mydb = None
 target = 'python_flask.db'
 
-# Create an Instance
 app = Flask(__name__)
 
+# Routes
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
 
-#Creating Routes
-@app.route('/users', methods=['POST'])
-def set_user():
-	user 		= request.json
-	
-	#key 
-	str2hash 	= user['email']+str(random.randint(100000,999999))
-	hashresult 	= hashlib.md5(str2hash.encode())
-	hashresult	= hashresult.hexdigest()
+@app.route('/form/user', methods=['GET'])
+def form_user():
+	return render_template('form_user.html')
 
-	mydb 	= sqlite3.connect(target)
-	sql 	= f"INSERT INTO users (name, email, key) VALUES ('{user['name']}','{user['email']}','{hashresult}')"
-	mycur 	= mydb.cursor()
+@app.route('/form/store', methods=['GET'])
+def form_store():
+	return render_template('form_store.html')
+
+@app.route('/form/product', methods=['GET'])
+def form_product():
+	return render_template('form_product.html')
+
+@app.route('/users', methods=['GET'])
+def get_user():
+	mydb = sqlite3.connect(target)
+	sql = f"SELECT id, name, email FROM users"
+	mycur = mydb.cursor()
 	mycur.execute(sql)
-	mydb.commit()
+	fech_users = mycur.fetchall()
 
-	return make_response(jsonify(	erro = '0',
-					msg = 'sucess',
-					key = hashresult)
-	)
+	users = list()
+	for user in fech_users:
+		users.append(
+			{
+				'id': user[0],
+				'name': user[1],
+				'email': user[2]
+			}
+		)
+	return (jsonify(users))
+
+@app.route('/form/set_user', methods=['POST'])
+def set_user():
+	if request.method == 'POST':
+		name = request.form['name']
+		email = request.form['email']
+
+		# Creating a key 
+		str2hash = email + str(random.randint(100000, 999999))
+		hashresult = hashlib.md5(str2hash.encode())
+		hashresult = hashresult.hexdigest()
+
+		mydb = sqlite3.connect(target)
+		sql = f"INSERT INTO users (name, email, key) VALUES ('{name}','{email}','{hashresult}')"
+		mycur = mydb.cursor()
+		mycur.execute(sql)
+		mydb.commit()
+
+		return redirect(url_for('index'))
 
 @app.route('/stores', methods=['GET'])
 def get_store():
-	mydb 		= sqlite3.connect(target)
-	sql 		= f"SELECT id, name FROM stores"
-	mycur 		= mydb.cursor()
+	mydb = sqlite3.connect(target)
+	sql = f"SELECT id, userId, name FROM stores"
+	mycur = mydb.cursor()
 	mycur.execute(sql)
-	fech_stores = mycur.fetchall()
+	fetch_stores = mycur.fetchall()
 
-	stores 		= list()
-	for store in fech_stores:
+	stores = list()
+	for store in fetch_stores:
 		stores.append(
 			{
-				'id': store[0],
-				'name': store[1],
+				'store_id': store[0],
+				'user_id': store[1],
+				'name': store[2],
 			}
 		)
-	return make_response(jsonify(	erro = '0',
-					msg = '',
-					data = stores)
-	)
+	return jsonify(stores)
 
-@app.route('/stores', methods=['POST'])
+@app.route('/form/set_store', methods=['POST'])
 def set_store():
-	store 		= request.json
-	
-	key		= store['key']
-	name    = store['name']
+	if request.method == 'POST':
+		store_name = request.form['store_name']
+		name = request.form['name']
+		email = request.form['email']
 
-	#getting the user id
-	mydb 	= sqlite3.connect(target)
-	mycur 	= mydb.cursor()
-	sql 	= f"SELECT id FROM users WHERE key='{key}' LIMIT 1"
-	mycur.execute(sql)
-	user 	= mycur.fetchone()
-	user_id	= user[0]
+		# Getting user id through name and email
+		mydb = sqlite3.connect(target)
+		mycur = mydb.cursor()
+		sql = f"SELECT id FROM users WHERE name='{name}' AND email='{email}'"
+		mycur.execute(sql)
+		user = mycur.fetchone()
+		user_id	= user[0]
 
-	sql 	= f"INSERT INTO stores (name, userId) VALUES ('{name}','{user_id}')"
-	mycur2 	= mydb.cursor()
-	mycur2.execute(sql)
-	mydb.commit()
+		sql = f"INSERT INTO stores (name, userId) VALUES ('{store_name}', '{user_id}')"
+		mycur2 	= mydb.cursor()
+		mycur2.execute(sql)
+		mydb.commit()
 
-	return make_response(jsonify(	erro = '0',
-									msg = 'sucess')
-	)
+		return redirect(url_for('index'))
 
 @app.route('/products', methods=['GET'])
 def get_product():
-	mydb 		= sqlite3.connect(target)
-	sql 		= f"SELECT id, name, price, (SELECT name FROM stores WHERE id = p.storeId) FROM products p"
-	mycur 		= mydb.cursor()
+	mydb = sqlite3.connect(target)
+	sql = f"SELECT id, name, price, storeId FROM products"
+	mycur = mydb.cursor()
 	mycur.execute(sql)
-	fech_products = mycur.fetchall()
+	fetch_products = mycur.fetchall()
 
-	products 		= list()
-	for product in fech_products:
+	products = list()
+	for product in fetch_products:
 		products.append(
 			{
 				'id': product[0],
 				'name': product[1],
 				'price': product[2],
-				'store': product[3]
+				'store_id': product[3]
 			}
 		)
-	return make_response(jsonify(	erro = '0',
-					msg = '',
-					data = products)
-	)
+	return jsonify(products)
 
-@app.route('/products', methods=['POST'])
+@app.route('/form/set_product', methods=['POST'])
 def set_product():
-	product 		= request.json
-	
-	storeId	= product['store']
-	name    = product['name']
-	price	= product['price']
+	if request.method == 'POST':
 
-	sql 	= f"INSERT INTO products (name, price, storeId) VALUES ('{name}','{price}','{storeId}')"
-	mydb 	= sqlite3.connect(target)
-	mycur 	= mydb.cursor()
-	mycur.execute(sql)
-	mydb.commit()
+		store_name = request.form['store_name']
+		product_name = request.form['product_name']
+		price = request.form['price']
 
-	return make_response(jsonify(	erro = '0',
-					msg = 'sucess')
-	)
+		mydb = sqlite3.connect(target)
+		sql = f"SELECT stores.id AS storeID FROM stores INNER JOIN users ON stores.userId = users.id WHERE stores.name='{store_name}'"
+		mycur = mydb.cursor()
+		mycur.execute(sql)
+		fetch_id = mycur.fetchone()
 
-@app.route('/test', methods=['GET'])
-def get_test():
-	return make_response(jsonify(	erro = '0',
-					msg = 'Test with GET sucess')
-	)
+		sql = f"INSERT INTO products (name, price, storeId) VALUES ('{product_name}','{price}','{fetch_id}')"
+		mycur2 = mydb.cursor()
+		mycur2.execute(sql)
+		mydb.commit()
 
-@app.route('/test', methods=['POST'])
-def set_test():
-	json = request
-	return make_response(jsonify(	erro = '0',
-					msg = 'Test with POST sucess')
-	)
+		return redirect(url_for('index'))
 
-# Starting the server 
-
-app.run()
+# Starting the server
+app.run(debug=True)
